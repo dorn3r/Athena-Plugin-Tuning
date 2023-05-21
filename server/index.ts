32aa1ba2c9950ec253a2c71333e1ecc39b52899c
dorn3r <54009081+dorn3r@users.noreply.github.com>
2023-05-21 13:24:54 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,10 @@ import * as alt from 'alt-server';
 import * as Athena from '@AthenaServer/api';
 import { TuningShopView } from './src/view';
 
-const PLUGIN_NAME = 'Alpha Tuning';
+const PLUGIN_NAME = 'Core Tuning';
 
 Athena.systems.plugins.registerPlugin(PLUGIN_NAME, () => {
     TuningShopView.init();
-    alt.log('Alpha Tuningmain betöltve!');
+    alt.log('Core Tuning Loaded!');
 });
 
